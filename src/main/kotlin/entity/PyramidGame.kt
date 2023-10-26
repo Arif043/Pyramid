903a727e10ff33a1c2118a56fe54b0ef303a219f
Arif Ertugrul <arif.ertugrul@outlook.com>
2023-10-26 20:46:51 +0200

    PyramidGame refactored

--- src/main/kotlin/entity/PyramidGame.kt
+++ src/main/kotlin/entity/PyramidGame.kt
@@ -2,5 +2,9 @@ package entity
 
 import tools.aqua.bgw.util.Stack
 
-data class PyramidGame(val drawStack: Stack<Card>, val reserveStack: Stack<Card>, val pyramid: HashMap<Int, Card>,
-                        val player1: Player, val player2: Player, val currentPlayer: Player)
+data class PyramidGame(val drawStack: Stack<Card>,
+                       val reserveStack: Stack<Card>,
+                       val pyramid: HashMap<Int, Card>,
+                       val player1: Player,
+                       val player2: Player,
+                       val currentPlayer: Player)
