450da5ae61e04dc62f8c993fe96da62136673a36
Arif Ertugrul <arif.ertugrul@outlook.com>
2023-10-27 21:26:39 +0200

    include Card documentation

--- src/main/kotlin/entity/PyramidGame.kt
+++ src/main/kotlin/entity/PyramidGame.kt
@@ -2,6 +2,10 @@ package entity
 
 import tools.aqua.bgw.util.Stack
 
+/**
+ * PyramidGame the root entity class.
+ * consists the other enitiy objects
+ */
 data class PyramidGame(val drawStack: Stack<Card>,
                        val reserveStack: Stack<Card>,
                        val pyramid: HashMap<Int, ArrayList<Card>>,
