dae6f4da67878a53821fddc3340420b1bddc17be
Arif Ertugrul <arif.ertugrul@outlook.com>
2023-10-27 15:43:57 +0200

    Updated entity implementation

--- src/main/kotlin/entity/Card.kt
+++ src/main/kotlin/entity/Card.kt
@@ -1,9 +1,19 @@
 package entity
 
-import tools.aqua.bgw.components.container.CardStack
+import java.util.Objects
 
 data class Card(val suit: CardSuit, val value :CardValue) {
     var revealed :Boolean = false
     var isReserveCard :Boolean = false
     val row: Int = 0
+
+    override fun toString() = "$suit:$value"
+    override fun hashCode() = Objects.hash(suit, value)
+
+    override fun equals(other: Any?): Boolean {
+        if (other is Card) {
+            return suit == other.suit && value == other.value
+        }
+        return false
+    }
 }
--- src/main/kotlin/entity/Player.kt
+++ src/main/kotlin/entity/Player.kt
@@ -4,4 +4,10 @@ data class Player(val selectedCards: Array<Card>) {
     var score: Int = 0
     var hasPressed: Boolean = false
     var playerName: String = ""
+
+    override fun toString() = playerName
+    override fun hashCode() = playerName.hashCode()
+    override fun equals(other: Any?): Boolean {
+        return other is Player && playerName == other.playerName
+    }
 }
