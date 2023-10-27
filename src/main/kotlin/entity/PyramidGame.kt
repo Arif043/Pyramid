c75452a403865981a59ed20d8250143e8625fd6f
arif.ertugrul <arif.ertugrul@tu-dortmund.de>
2023-10-27 21:50:58 +0200

    Include Player documentation

--- src/main/kotlin/entity/PyramidGame.kt
+++ src/main/kotlin/entity/PyramidGame.kt
@@ -4,7 +4,15 @@ import tools.aqua.bgw.util.Stack
 
 /**
  * PyramidGame the root entity class.
- * consists the other enitiy objects
+ * consists the other entity objects
+ *
+ * @property drawStack a stack which provides cards for the player
+ * @property reserveStack a stack which contains drawn cards from the drawStack
+ * @property pyramid a hashmap which represents the pyramid in the game.
+ *           The key is the row number. Each of the rows have an own list.
+ * @property player1 the first player
+ * @property player2 the second player
+ * @property currentPlayer a reference that contains the currentplayer
  */
 data class PyramidGame(val drawStack: Stack<Card>,
                        val reserveStack: Stack<Card>,
