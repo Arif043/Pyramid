734285a28d5f2ff0b5391088e0ae37b72906ced7
Arif Ertugrul <arif.ertugrul@outlook.com>
2023-11-13 20:22:47 +0100

    Updated NewGameScene

--- src/main/kotlin/view/NewGameScene.kt
+++ src/main/kotlin/view/NewGameScene.kt
@@ -1,27 +1,58 @@
 package view
 
+import tools.aqua.bgw.components.uicomponents.Button
 import tools.aqua.bgw.components.uicomponents.Label
 import tools.aqua.bgw.components.uicomponents.TextField
 import tools.aqua.bgw.core.MenuScene
 import tools.aqua.bgw.util.Font
+import tools.aqua.bgw.visual.ColorVisual
 
-class NewGameScene : MenuScene(), Refreshable {
+class NewGameScene : MenuScene(270, 500), Refreshable {
+
+    private val fontSize = 13
 
     val player1NameLabel = Label(
         text = "Player 1",
-        posX = 10,
         posY = 10,
-        font = Font(26)
+        font = Font(fontSize)
     )
     val player1NameField = TextField(
-        posY = player1NameLabel.posY + player1NameLabel.height + 15,
-        font = Font(26)
+        posY = player1NameLabel.posY + player1NameLabel.height,
+        font = Font(fontSize),
+    )
+    val player2NameLabel = Label(
+        text = "Player 2",
+        posY = player1NameField.posY + player1NameField.height + 80,
+        font = Font(fontSize)
+    )
+    val player2NameField = TextField(
+        posY = player2NameLabel.posY + player2NameLabel.height,
+        font = Font(fontSize)
+    )
+    val quitButton = Button(
+        text = "Quit",
+        visual = ColorVisual.RED,
+        posY = player2NameField.posY + 180,
+        font = Font(fontSize)
+    )
+    val startButton = Button(
+        text = "Start",
+        visual = ColorVisual.GREEN,
+        posY = quitButton.posY,
+        posX = quitButton.width + 30,
+        font = Font(fontSize)
     )
 
     init {
+        player1NameField.width *= 1.9
+        player2NameField.width *= 1.9
         addComponents(
             player1NameLabel,
-            player1NameField
+            player1NameField,
+            player2NameLabel,
+            player2NameField,
+            quitButton,
+            startButton
         )
     }
 
