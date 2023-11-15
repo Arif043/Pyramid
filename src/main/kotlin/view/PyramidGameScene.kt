472f2ec49e72571b9fbb0fc226b3396b3b5dbef9
arif.ertugrul <arif.ertugrul@tu-dortmund.de>
2023-11-15 16:17:14 +0100

    Added Reservestack in GUI

--- src/main/kotlin/view/PyramidGameScene.kt
+++ src/main/kotlin/view/PyramidGameScene.kt
@@ -12,23 +12,35 @@ import tools.aqua.bgw.visual.ImageVisual
 class PyramidGameScene(private val rootService: RootService) : BoardGameScene(background = ColorVisual.ORANGE),
     Refreshable {
 
-    val c = CardStack<CardView>(10, 10)
+    val drawStack = CardStack<CardView>(100, 100)
+    val reserveStack = CardStack<CardView>(500, 100)
 
     init {
         buildDrawStack()
+        //buildReserveStack()
+        onKeyPressed = {
+            drawStack.pop()
+        }
         //val cc = CardView(front = ImageVisual(getCardViewFrom(rootService.currentGame.drawStack.peek())))
 
-        addComponents(c)
-        print(c.)
+        addComponents(
+            drawStack,
+            reserveStack)
     }
 
     private fun buildDrawStack() {
         val drawStackCards = rootService.currentGame.drawStack.peekAll()
         for (i in 0..drawStackCards.lastIndex) {
-            c.add(getCardViewFrom(drawStackCards[i]), i)
+            drawStack.add(getCardViewFrom(drawStackCards[i]), i)
         }
-
     }
+
+    /*private fun buildReserveStack() {
+        val reserveStackCards = rootService.currentGame.reserveStack.peekAll()
+        for (i in 0..reserveStackCards.lastIndex) {
+            reserveStack.add(getCardViewFrom(reserveStackCards[i]), i)
+        }
+    }*/
 
     private fun getCardViewFrom(card: Card) : CardView {
         val loader = CardImageLoader()
